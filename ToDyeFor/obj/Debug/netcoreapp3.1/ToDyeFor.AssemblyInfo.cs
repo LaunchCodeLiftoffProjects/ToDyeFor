12fd4b8f0c87c4a8921700c7a3ca0f98c47bd4eb
Pearce Odegard <pearceodegard@gmail.com>
2021-02-01 21:29:42 -0600

    Deleted/reorganized files, created .gitignore, recreated project

--- ToDyeFor/obj/Debug/netcoreapp3.1/ToDyeFor.AssemblyInfo.cs
+++ ToDyeFor/obj/Debug/netcoreapp3.1/ToDyeFor.AssemblyInfo.cs
@@ -11,7 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-ToDyeFor-75814EB2-72EA-420B-A762-97FC6A09585E")]
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-ToDyeFor-4D504F2D-0249-458C-AE42-0008E4ED6179")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDyeFor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]

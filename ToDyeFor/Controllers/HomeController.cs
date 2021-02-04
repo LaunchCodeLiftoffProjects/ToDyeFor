3652dee40665cb56115033ca79accfee2e7cf5cd
Taylor MoJo <empyros.enterprises@gmail.com>
2021-02-04 14:00:01 -0600

    updated basic version of calculator form

--- ToDyeFor/Controllers/HomeController.cs
+++ ToDyeFor/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace ToDyeFor.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }

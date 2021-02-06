a93f89e36ce9de398eea0c5ea10464d6fe16983f
Rachel Rolon <66883810+rrolon47@users.noreply.github.com>
2021-02-06 17:05:08 -0600

    updated User class and created a UserMXRecipe class to create a relationship between the two.

--- ToDyeFor/Models/User.cs
+++ ToDyeFor/Models/User.cs
@@ -7,9 +7,9 @@ namespace ToDyeFor.Models
 {
     public class User
     {
-        public int UserId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
-        public List<string> Recipes { get; set; }
+        public List<UserRecipe> UserRecipes { get; set; }
         
         //public string Picture { get; set; }
 
---
+++ ToDyeFor/Models/UserRecipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDyeFor.Models
+{
+    public class UserRecipe
+    {
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        public  int  MXRecipeId { get; set; }
+
+        public MXRecipe MXRecipe { get; set; }
+    }
+}

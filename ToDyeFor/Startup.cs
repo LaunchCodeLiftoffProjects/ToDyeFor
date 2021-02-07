ceb5465996fd7977e1578083eb1e4aed12c455df
Pearce Odegard <pearceodegard@gmail.com>
2021-02-07 00:46:57 -0600

    Added reference to azure secret

--- ToDyeFor/Startup.cs
+++ ToDyeFor/Startup.cs
@@ -29,7 +29,7 @@ namespace ToDyeFor
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("ConnectionStrings:ToDyeForConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();

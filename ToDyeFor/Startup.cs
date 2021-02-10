efce7516ec1fce8a8272d7309919f89787367177
Megan Orton <megan.orton0@gmail.com>
2021-02-09 20:25:28 -0600

    rebranched from main and extended identity class

--- ToDyeFor/Startup.cs
+++ ToDyeFor/Startup.cs
@@ -12,6 +12,7 @@ using ToDyeFor.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ToDyeFor.Models;
 
 namespace ToDyeFor
 {
@@ -30,7 +31,7 @@ namespace ToDyeFor
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();

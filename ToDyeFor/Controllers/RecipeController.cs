5466c462c061cdd42372e5bc55c5f199faf35576
Rachel Rolon <66883810+rrolon47@users.noreply.github.com>
2021-02-11 21:42:36 -0600

    Created MVC connected  with model binding.  Still need to finish model binding in delete and edit.

---
+++ ToDyeFor/Controllers/RecipeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDyeFor.Data;
+using ToDyeFor.Models;
+using ToDyeFor.ViewModel;
+
+namespace ToDyeFor.Controllers
+{
+    public class RecipeController : Controller
+    {
+        static private List<MXRecipe> MXRecipes = new List<MXRecipe>();
+
+        //get: Recipe
+        public IActionResult Index()
+        {
+            List<MXRecipe> mxRecipes = new List<MXRecipe>(RecipeData.GetAll());
+
+            return View(mxRecipes);
+        }
+
+        //retrieves form
+        [HttpGet]
+        [Route("Recipe/Calculator")]
+        public IActionResult Calculator()
+        {
+            calculateMXRecipeViewModel calculateMXRecipeViewModel = new calculateMXRecipeViewModel();
+            return View(calculateMXRecipeViewModel);
+        }
+
+        //processes form
+        [HttpPost]
+        [Route("Recipe/Calculator")]
+        public IActionResult Calculator(calculateMXRecipeViewModel calculateMXRecipeViewModel)
+        {
+            MXRecipe newMXRecipe = new MXRecipe
+            {
+                Name = calculateMXRecipeViewModel.Name,
+                DyeColor = calculateMXRecipeViewModel.DyeColor
+            };
+            RecipeData.Add(newMXRecipe);
+            return Redirect("/Recipe");
+        }
+
+        [HttpGet]
+        [Route("Recipe/Delete")]
+        public IActionResult Delete()
+        {
+            ViewBag.mxrecipes = RecipeData.GetAll();
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Recipe/Delete")]
+        public IActionResult Delete(int[] recipeIds)
+        {
+            foreach (int recipeId in recipeIds)
+            {
+                RecipeData.Remove(recipeId);
+            }
+
+            return Redirect("/Events");
+        }
+
+        //get: Recipe/edit/eventId
+        [HttpGet]
+        [Route("/Recipe/Edit/{recipeId}")]
+        public IActionResult Edit(int recipeId)
+        {
+            MXRecipe recipeById = RecipeData.GetById(recipeId);
+            ViewBag.editRecipe = recipeById;
+            ViewBag.title = $"Edit Event {recipeById.Name}  (id={recipeById.Id})";
+            return View();
+        }
+
+        //processes form
+        [HttpPost]
+        [Route("/Recipe/Edit")]
+        public IActionResult Edit(int recipeId, string name, string dyeColor)
+        {
+            MXRecipe recipeById = RecipeData.GetById(recipeId);
+            recipeById.Name = name;
+            recipeById.DyeColor = dyeColor;
+            return Redirect("/Recipe");
+
+        }
+    }
+}
---
+++ ToDyeFor/Data/RecipeData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDyeFor.Models;
+
+namespace ToDyeFor.Data
+{
+    public class RecipeData
+    {
+        static private Dictionary<int, MXRecipe> MXRecipes = new Dictionary<int, MXRecipe>();
+
+        // GetAll
+        public static IEnumerable<MXRecipe> GetAll()
+        {
+            return MXRecipes.Values;
+        }
+
+        // Add
+        public static void Add(MXRecipe newMXRecipe)
+        {
+            MXRecipes.Add(newMXRecipe.Id, newMXRecipe);
+        }
+
+        // Remove
+        public static void Remove(int id)
+        {
+            MXRecipes.Remove(id);
+        }
+
+        // GetById
+        public static MXRecipe GetById(int id)
+        {
+            return MXRecipes[id];
+        }
+    }
+}

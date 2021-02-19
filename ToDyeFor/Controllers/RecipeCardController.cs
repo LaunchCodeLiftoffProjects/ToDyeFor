cb4bcb7f1c6e9ec5cd56c29a3837fca9a4d881ae
Pearce Odegard <pearceodegard@gmail.com>
2021-02-18 19:02:44 -0600

    Started work on test recipe card

---
+++ ToDyeFor/Controllers/RecipeCardController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDyeFor.Data;
+using ToDyeFor.Models;
+
+namespace ToDyeFor.Controllers
+{
+    public class RecipeCardController : Controller
+    {
+        static private List<MXRecipe> MXRecipes = new List<MXRecipe>();
+        private ApplicationDbContext context;
+        private List<MXRecipe> mxRecipes;
+
+        public RecipeCardController(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+            mxRecipes = context.MXRecipes.ToList();
+        }
+        // GET: HomeController1
+        public ActionResult Index()
+        {
+            return View(mxRecipes);
+        }
+
+        // GET: HomeController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

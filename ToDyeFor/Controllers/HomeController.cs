f5cb9a39fd87ccd5adf55dacd8d6e501af9565bb
Taylor MoJo <empyros.enterprises@gmail.com>
2021-02-18 19:42:21 -0600

    added model binding

--- ToDyeFor/Controllers/HomeController.cs
+++ ToDyeFor/Controllers/HomeController.cs
@@ -5,22 +5,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ToDyeFor.Data;
 using ToDyeFor.Models;
+using ToDyeFor.ViewModel;
 
 namespace ToDyeFor.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        static private List<MXRecipe> MXRecipes = new List<MXRecipe>();
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            calculateMXRecipeViewModel calculateMXRecipeViewModel = new calculateMXRecipeViewModel();
+            return View(calculateMXRecipeViewModel);
+        }
+
+        [HttpPost]
+        [Route("Home/Results")]
+        public IActionResult Index(calculateMXRecipeViewModel calculateMXRecipeViewModel)
+        {
+            MXRecipe newMXRecipe = new MXRecipe
+            {
+                Name = calculateMXRecipeViewModel.Name,
+                DyeColor = calculateMXRecipeViewModel.DyeColor,
+                ShadeDepth = calculateMXRecipeViewModel.ShadeDepth,
+                FabricWeight = calculateMXRecipeViewModel.FabricWeight,
+                Salt = calculateMXRecipeViewModel.Salt(),
+                SodaAsh = calculateMXRecipeViewModel.SodaAsh(),
+                Water = calculateMXRecipeViewModel.Water(),
+                Dye = calculateMXRecipeViewModel.Dye()
+            };
+            RecipeData.Add(newMXRecipe);
+
+            return Redirect("/Home/Results");
+        }
+
+        public IActionResult Results()
+        {
+            List<MXRecipe> mxRecipes = new List<MXRecipe>(RecipeData.GetAll());
+
+            return View(mxRecipes);
         }
 
         public IActionResult About()
--- ToDyeFor/wwwroot/js/site.js
+++ ToDyeFor/wwwroot/js/site.js
@@ -2,3 +2,116 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/*
+    var currentTab = 0; // Current tab is set to be the first tab (0)
+    showTab(currentTab); // Display the current tab
+
+        function showTab(n) {
+            // This function will display the specified tab of the form ...
+            var x = document.getElementsByClassName("tab");
+            x[n].style.display = "block";
+            // ... and fix the Previous/Next buttons:
+            if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+            } else {
+        document.getElementById("prevBtn").style.display = "inline";
+            }
+            if (n == (x.length -1)) {
+        document.getElementById("nextBtn").innerHTML = "Submit";
+                document.getElementById("nextBtn").value = "Submit";
+
+            } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+            }
+            // ... and run a function that displays the correct step indicator:
+            fixStepIndicator(n)
+        }
+
+        function nextPrev(n) {
+            // This function will figure out which tab to display
+            var x = document.getElementsByClassName("tab");
+            // Exit the function if any field in the current tab is invalid:
+            if (n == 1 && !validateForm()) return false;
+            // Hide the current tab:
+            x[currentTab].style.display = "none";
+            // Increase or decrease the current tab by 1:
+            currentTab = currentTab + n;
+            // if you have reached the end of the form... :
+            if (currentTab >= x.length) {
+        //...the form gets submitted:
+        //need to connect to database here?
+        document.getElementById("nextBtn").onclick = submitForm();
+                document.getElementById("calculator").submitForm();
+
+                return false;
+            }
+            // Otherwise, display the correct tab:
+            showTab(currentTab);
+        }
+
+
+
+        function validateForm() {
+            // This function deals with validation of the form fields
+            var x, y, i, valid = true;
+            x = document.getElementsByClassName("tab");
+            y = x[currentTab].getElementsByTagName("input");
+            // A loop that checks every input field in the current tab:
+            for (i = 0; i < y.length; {
+                // If a field is empty...
+                if (y[i].value == "") {
+        // add an "invalid" class to the field:
+        y[i].className += " invalid";
+                    // and set the current valid status to false:
+                    valid = false;
+                }
+            }
+            // If the valid status is true, mark the step as finished and valid:
+            if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+            }
+            return valid; // return the valid status
+        }
+
+        function fixStepIndicator(n) {
+            // This function removes the "active" class of all steps...
+            var i, x = document.getElementsByClassName("step");
+            for (i = 0; i < x.length; {
+        x[i].className = x[i].className.replace(" active", "");
+            }
+            //... and adds the "active" class to the current step:
+            x[n].className += " active";
+        }
+
+        // displays current value of sliders
+
+        @*var slider1 = document.getElementById("quantity");
+        var output1 = document.getElementById("amount");
+
+        output1.innerHTML = slider1.value;
+        slider1.oninput = function () {
+        output1.innerHTML = this.value;
+        }*@
+
+        var slider2 = document.getElementById("shade");
+        var output2 = document.getElementById("depth");
+        output2.innerHTML = slider2.value;
+        slider2.oninput = function () {
+        output2.innerHTML = this.value;
+        }
+
+        // what happens when they click the submit button
+        function submitForm() {
+        let confirmation = "You've submitted the following: ";
+            let a = document.getElementById("name").value;
+            let b = document.getElementById("fabric").value;
+            let c = document.getElementById("dye").value;
+            let d = document.getElementById("color").value;
+            let e = document.getElementById("quantity").value;
+            let f = document.getElementById("depth").value;
+            alert(confirmation + " " + a + ", " + b + ", " + c + ", " + d + ", " + e + ", " + f);
+        }
+
+
+    */

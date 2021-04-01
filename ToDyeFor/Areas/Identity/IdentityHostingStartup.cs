19f6b78f560b8fe09c4f94fccb0bbc602ba80bd8
Rachel Rolon <66883810+rrolon47@users.noreply.github.com>
2021-04-01 17:34:02 -0500

    scaffolded in all of the log in and user account setting pages

---
+++ ToDyeFor/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDyeFor.Data;
+using ToDyeFor.Models;
+
+[assembly: HostingStartup(typeof(ToDyeFor.Areas.Identity.IdentityHostingStartup))]
+namespace ToDyeFor.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
